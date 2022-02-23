708ec316dc8af90399cf985c275e4b242753708e
Ben Lorenz <ben.lorenz@bsi.bund.de>
2022-02-23 18:02:35 +0100

    fixed case nr

--- shutdown_dialog.sh
+++ shutdown_dialog.sh
@@ -9,7 +9,7 @@ fi
 
 cowsay_msg=$(echo "$msg" | cowsay -f $(ls /usr/share/cows/ | shuf -n1))
 cowsay_msg_n=$(printf "$cowsay_msg\n.")
-gxmessage "$cowsay_msg_n" -center -title "Shutdown" -font "monospace 10" -default "Cancel" -buttons "_Cancel":2,"_Logout":2,"_Reboot":3,"_Shutdown":4 >/dev/null
+gxmessage "$cowsay_msg_n" -center -title "Shutdown" -font "monospace 10" -default "Cancel" -buttons "_Cancel":1,"_Logout":2,"_Reboot":3,"_Shutdown":4 >/dev/null
 
 case $? in
   1)
