3dbdf93d41ca69a67bb0f0d93b47b134eb88ca51
Ben Lorenz <ben.lorenz@bsi.bund.de>
2022-02-23 17:53:44 +0100

    added offline case

--- shutdown_dialog.sh
+++ shutdown_dialog.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 msg=$(wget "http://api.icndb.com/jokes/random" -qO- | jshon -e value -e joke -u | recode html)
+
+if [ -z "$msg" ]
+    then
+    msg="You seem to be offline..."
+fi
+
 cowsay_msg=$(echo "$msg" | cowsay -f $(ls /usr/share/cows/ | shuf -n1))
 cowsay_msg_n=$(printf "$cowsay_msg\n.")
 gxmessage "$cowsay_msg_n" -center -title "Shutdown" -font "monospace 10" -default "Cancel" -buttons "_Cancel":2,"_Logout":2,"_Reboot":3,"_Shutdown":4 >/dev/null
