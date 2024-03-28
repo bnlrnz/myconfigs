6687251069e87ccf1e441c6db39695fb8cebb97a
Ben Lorenz <ben.lorenz@bsi.bund.de>
2024-03-28 10:50:10 +0100

    define term env var and nvim alias

--- fish/config.fish
+++ fish/config.fish
@@ -2,11 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Start X at login
+# Start Hyprland at login
 if status --is-login
   if test -z "$DISPLAY" -a $XDG_VTNR = 1
-    exec startx -- -keeptty
+    exec Hyprland
   end
 end
+
+export TERM=xterm-256color
+
+alias vim="nvim" 
 
 fish_ssh_agent
