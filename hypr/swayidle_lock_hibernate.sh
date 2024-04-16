6f28497b2d51e4e8e558770443cf2dbe4d4baa99
Ben Lorenz <bnlrnz@gmail.com>
2024-04-16 08:34:41 +0200

    fixed swayidle script

--- hypr/swayidle_lock_hibernate.sh
+++ hypr/swayidle_lock_hibernate.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ "$HOSTNAME" = "nix" ]; then
+if [ $(hostname) = "nix" ]; then
   swayidle -w \
-    timeout 1200 'systemctl suspend' \
+    timeout 1200 'systemctl suspend' 
 else
   swayidle -w \
     timeout 300 'swaylock -f --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 20 --fade-in 0.2'\
