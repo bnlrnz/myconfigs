41f053f18f76369a7af613e72fff6d92ff4c791b
Ben Lorenz <bnlrnz@gmail.com>
2024-08-13 22:21:28 +0200

    added mangohud

--- nix_files/hardware-configuration_nix-home.nix
+++ nix_files/hardware-configuration_nix-home.nix
@@ -92,6 +92,11 @@
   programs.steam.gamescopeSession.enable = true;
   programs.gamemode.enable = true;
 
+  environment.systemPackages = with pkgs; [
+    mangohud
+    goverlay
+  ];
+
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = 
       "\${HOME}/.steam/root/compatibilitytools.d";
