ba3f9410fcd40dd7e49385a653033d4f9e7ee5f6
Ben Lorenz <bnlrnz@gmail.com>
2024-08-18 22:10:18 +0200

    immutable resolv.conf

--- nix_files/nix-thinkpad.nix
+++ nix_files/nix-thinkpad.nix
@@ -50,6 +50,15 @@ in {
     allowedUDPPorts = [ 4431 ]; # openfortivpn -> 4431
   };
 
+  # Generate an immutable /etc/resolv.conf from the nameserver settings
+  # above (otherwise DHCP overwrites it):
+  environment.etc."resolv.conf" = with lib; with pkgs; {
+    source = writeText "resolv.conf" ''
+      ${concatStringsSep "\n" (map (ns: "nameserver ${ns}") config.networking.nameservers)}
+      options wlp0s20f3
+    '';
+  };
+
   # ctf hosts
   networking.extraHosts =
     ''
