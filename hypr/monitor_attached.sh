d82c7363911cca6a7935ff9d1bb7f5f801a0b663
Ben Lorenz <bnlrnz@gmail.com>
2025-04-02 09:06:20 +0200

    force activating display if in sleep mode

--- hypr/monitor_attached.sh
+++ hypr/monitor_attached.sh
@@ -4,5 +4,6 @@ hyprctl dispatch moveworkspacetomonitor 1 "$1"
 hyprctl dispatch workspace 1
 hyprctl dispatch moveworkspacetomonitor 3 "$1"
 
+hyprctl dispatch dpms on
 pkill -f wpaperd; wpaperd &
 pkill -f waybar; waybar &
