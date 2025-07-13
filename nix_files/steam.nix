7972f75852b64a3a7e3251a30c281ec638ac76b1
Ben Lorenz <bnlrnz@gmail.com>
2025-07-13 21:39:15 +0200

    added lutris, heroic and stuff

--- nix_files/steam.nix
+++ nix_files/steam.nix
@@ -4,14 +4,23 @@
   programs.steam.enable = true;
   programs.steam.gamescopeSession.enable = true;
   programs.gamemode.enable = true;
+  programs.gamescope.enable = true;
 
   environment.systemPackages = with pkgs; [
     mangohud
     goverlay
+    protonup-qt
+    lutris
+    heroic
+    winetricks
+    protontricks
+    wineWowPackages.waylandFull
   ];
 
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = 
       "\${HOME}/.steam/root/compatibilitytools.d";
   };
+
+
 }
