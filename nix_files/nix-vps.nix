b6c56dbc001046e35354d3fb6ebd073657d677b9
ben@nix-vps <bnlrnz@gmail.com>
2025-07-22 13:17:40 +0200

    added n8n

--- nix_files/nix-vps.nix
+++ nix_files/nix-vps.nix
@@ -172,6 +172,7 @@ in {
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "corefonts"
+    "n8n"
   ];
 
   # List packages installed in system profile. To search, run:
@@ -329,6 +330,9 @@ in {
           header_up Upgrade {>Upgrade}
           header_up Connection {>Connection}
         }
+    '';
+    virtualHosts."n8n.b3lo.de".extraConfig = ''
+      reverse_proxy http://localhost:5678
     '';
   }; 
 
@@ -491,6 +495,18 @@ in {
       "listen.group" = "caddy";
       "listen.mode" = "0600";
     };
+  };
+
+  ##################
+  # n8n
+  ##################
+  services.n8n = {
+    enable = true;
+    openFirewall = true;
+    webhookUrl = "n8n.b3lo.de";
+    # default port is 5678
+    # due to out of memory error, build needed to run with this:
+    # NODE_OPTIONS='--max-old-space-size=2000' sudo nixos-rebuild switch --upgrade
   };
 
   # Copy the NixOS configuration file and link it from the resulting system
