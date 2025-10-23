049b2e361d3ecd0c561f38bfc52a81fc5f89b002
Ben Lorenz <bnlrnz@gmail.com>
2025-10-23 22:14:37 +0200

    may improve erratic/random stuttering/fps drops

--- nix_files/hardware-configuration_nix-home.nix
+++ nix_files/hardware-configuration_nix-home.nix
@@ -9,6 +9,7 @@
   boot.initrd.availableKernelModules =
     [ "nvme" "ahci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.kernelParams = [ "amdgpu.gfxoff=0" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
   boot.supportedFilesystems = [ ];
