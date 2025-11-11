f69a38ce01cf2ac274ec5b795e088b05130df2c7
Ben Lorenz <bnlrnz@gmail.com>
2025-11-11 15:34:40 +0100

    improved script

--- nix_files/copy_from_etc_nixos.sh
+++ nix_files/copy_from_etc_nixos.sh
@@ -2,43 +2,164 @@
 
 hn=$(hostname)
 
-echo "Copying files for $hn"
+echo "Copying files for $hn (from etc to this folder)"
+echo "If you don't see further output, nothing changed."
 
 if [ "$hn" == "nix" ]; then
-    sudo cp -v /etc/nixos/configuration.nix nix-home.nix
-    sudo cp -v /etc/nixos/hardware-configuration_nix-home.nix hardware-configuration_nix-home.nix
-    sudo cp -v /etc/nixos/packages.nix packages.nix
-    sudo cp -v /etc/nixos/services.nix services.nix
-    sudo cp -v /etc/nixos/steam.nix steam.nix
-    sudo cp -v /etc/nixos/pwn.nix pwn.nix
-    sudo cp -v /etc/nixos/k3s.nix k3s.nix
-    sudo cp -v /etc/nixos/temis.nix temis.nix
-    sudo cp -v /etc/nixos/podman.nix podman.nix
+    if ! diff /etc/nixos/configuration.nix nix-home.nix > /dev/null ; then
+        diff /etc/nixos/configuration.nix nix-home.nix
+        read -n 1 -s -r -p "Press any key to overwrite nix-home.nix"
+        sudo cp -v /etc/nixos/configuration.nix nix-home.nix
+    fi
+    
+    if ! diff /etc/nixos/hardware-configuration_nix-home.nix hardware-configuration_nix-home.nix > /dev/null ; then
+        diff /etc/nixos/hardware-configuration_nix-home.nix hardware-configuration_nix-home.nix
+        read -n 1 -s -r -p "Press any key to overwrite hardware-configuration_nix-home.nix"
+        sudo cp -v /etc/nixos/hardware-configuration_nix-home.nix hardware-configuration_nix-home.nix
+    fi
+    
+    if ! diff /etc/nixos/packages.nix packages.nix > /dev/null ; then
+        diff /etc/nixos/packages.nix packages.nix
+        read -n 1 -s -r -p "Press any key to overwrite packages.nix"
+        sudo cp -v /etc/nixos/packages.nix packages.nix
+    fi
+    
+    if ! diff /etc/nixos/services.nix services.nix > /dev/null ; then
+        diff /etc/nixos/services.nix services.nix
+        read -n 1 -s -r -p "Press any key to overwrite services.nix"
+        sudo cp -v /etc/nixos/services.nix services.nix
+    fi
+    
+    if ! diff /etc/nixos/steam.nix steam.nix > /dev/null ; then
+        diff /etc/nixos/steam.nix steam.nix
+        read -n 1 -s -r -p "Press any key to overwrite steam.nix"
+        sudo cp -v /etc/nixos/steam.nix steam.nix
+    fi
+    
+    if ! diff /etc/nixos/pwn.nix pwn.nix > /dev/null ; then
+        diff /etc/nixos/pwn.nix pwn.nix
+        read -n 1 -s -r -p "Press any key to overwrite pwn.nix"
+        sudo cp -v /etc/nixos/pwn.nix pwn.nix
+    fi
+    
+    if ! diff /etc/nixos/k3s.nix k3s.nix > /dev/null ; then
+        diff /etc/nixos/k3s.nix k3s.nix
+        read -n 1 -s -r -p "Press any key to overwrite k3s.nix"
+        sudo cp -v /etc/nixos/k3s.nix k3s.nix
+    fi
+    
+    if ! diff /etc/nixos/temis.nix temis.nix > /dev/null ; then
+        diff /etc/nixos/temis.nix temis.nix
+        read -n 1 -s -r -p "Press any key to overwrite temis.nix"
+        sudo cp -v /etc/nixos/temis.nix temis.nix
+    fi
+    
+    if ! diff /etc/nixos/podman.nix podman.nix > /dev/null ; then
+        diff /etc/nixos/podman.nix podman.nix
+        read -n 1 -s -r -p "Press any key to overwrite podman.nix"
+        sudo cp -v /etc/nixos/podman.nix podman.nix
+    fi
+    
 fi
 
 if [ "$hn" == "tp-belo" ]; then
-    sudo cp -v /etc/nixos/configuration.nix nix-thinkpad.nix
-    sudo cp -v /etc/nixos/hardware-configuration_nix-thinkpad.nix hardware-configuration_nix-thinkpad.nix
-    sudo cp -v /etc/nixos/packages.nix packages.nix
-    sudo cp -v /etc/nixos/services.nix services.nix
-    sudo cp -v /etc/nixos/pwn.nix pwn.nix
-    sudo cp -v /etc/nixos/temis.nix temis.nix
-    sudo cp -v /etc/nixos/k3s.nix k3s.nix
-    sudo cp -v /etc/nixos/podman.nix podman.nix
+    if ! diff /etc/nixos/configuration.nix nix-thinkpad.nix > /dev/null ; then
+        diff /etc/nixos/configuration.nix nix-thinkpad.nix
+        read -n 1 -s -r -p "Press any key to overwrite nix-thinkpad.nix"
+        sudo cp -v /etc/nixos/configuration.nix nix-thinkpad.nix
+    fi
+    
+    if ! diff /etc/nixos/hardware-configuration_nix-thinkpad.nix hardware-configuration_nix-thinkpad.nix > /dev/null ; then
+        diff /etc/nixos/hardware-configuration_nix-thinkpad.nix hardware-configuration_nix-thinkpad.nix
+        read -n 1 -s -r -p "Press any key to overwrite hardware-configuration_nix-thinkpad.nix"
+        sudo cp -v /etc/nixos/hardware-configuration_nix-thinkpad.nix hardware-configuration_nix-thinkpad.nix
+    fi
+    
+    if ! diff /etc/nixos/packages.nix packages.nix > /dev/null ; then
+        diff /etc/nixos/packages.nix packages.nix
+        read -n 1 -s -r -p "Press any key to overwrite packages.nix"
+        sudo cp -v /etc/nixos/packages.nix packages.nix
+    fi
+    
+    if ! diff /etc/nixos/services.nix services.nix > /dev/null ; then
+        diff /etc/nixos/services.nix services.nix
+        read -n 1 -s -r -p "Press any key to overwrite services.nix"
+        sudo cp -v /etc/nixos/services.nix services.nix
+    fi
+    
+    if ! diff /etc/nixos/pwn.nix pwn.nix > /dev/null ; then
+        diff /etc/nixos/pwn.nix pwn.nix
+        read -n 1 -s -r -p "Press any key to overwrite pwn.nix"
+        sudo cp -v /etc/nixos/pwn.nix pwn.nix
+    fi
+    
+    if ! diff /etc/nixos/temis.nix temis.nix > /dev/null ; then
+        diff /etc/nixos/temis.nix temis.nix
+        read -n 1 -s -r -p "Press any key to overwrite temis.nix"
+        sudo cp -v /etc/nixos/temis.nix temis.nix
+    fi
+    
+    if ! diff /etc/nixos/k3s.nix k3s.nix > /dev/null ; then
+        diff /etc/nixos/k3s.nix k3s.nix
+        read -n 1 -s -r -p "Press any key to overwrite k3s.nix"
+        sudo cp -v /etc/nixos/k3s.nix k3s.nix
+    fi
+    
+    if ! diff /etc/nixos/podman.nix podman.nix > /dev/null ; then
+        diff /etc/nixos/podman.nix podman.nix
+        read -n 1 -s -r -p "Press any key to overwrite podman.nix"
+        sudo cp -v /etc/nixos/podman.nix podman.nix
+    fi
+    
 fi
 
 if [ "$hn" == "nix-vps" ]; then
-    sudo cp -v /etc/nixos/configuration.nix nix-vps.nix
-    sudo cp -v /etc/nixos/hardware-configuration_nix-vps.nix hardware-configuration_nix-vps.nix
-    sudo cp -v /etc/nixos/sa3_document_manager.nix sa3_document_manager.nix
+    if ! diff /etc/nixos/configuration.nix nix-vps.nix > /dev/null ; then
+        diff /etc/nixos/configuration.nix nix-vps.nix
+        read -n 1 -s -r -p "Press any key to overwrite nix-vps.nix"
+        sudo cp -v /etc/nixos/configuration.nix nix-vps.nix
+    fi
+    
+    if ! diff /etc/nixos/hardware-configuration_nix-vps.nix hardware-configuration_nix-vps.nix > /dev/null ; then
+        diff /etc/nixos/hardware-configuration_nix-vps.nix hardware-configuration_nix-vps.nix
+        read -n 1 -s -r -p "Press any key to overwrite hardware-configuration_nix-vps.nix"
+        sudo cp -v /etc/nixos/hardware-configuration_nix-vps.nix hardware-configuration_nix-vps.nix
+    fi
+    
+    if ! diff /etc/nixos/sa3_document_manager.nix sa3_document_manager.nix > /dev/null ; then
+        diff /etc/nixos/sa3_document_manager.nix sa3_document_manager.nix
+        read -n 1 -s -r -p "Press any key to overwrite sa3_document_manager.nix"
+        sudo cp -v /etc/nixos/sa3_document_manager.nix sa3_document_manager.nix
+    fi
+    
 fi
 
 if [ "$hn" == "nix-pi" ]; then
-    sudo cp -v /etc/nixos/configuration.nix nix-pi.nix
-    sudo cp -v /etc/nixos/hardware-configuration.nix hardware-configuration_nix-pi.nix
+    if ! diff /etc/nixos/configuration.nix nix-pi.nix > /dev/null ; then
+        diff /etc/nixos/configuration.nix nix-pi.nix
+        read -n 1 -s -r -p "Press any key to overwrite nix-pi.nix"
+        sudo cp -v /etc/nixos/configuration.nix nix-pi.nix
+    fi
+    
+    if ! diff /etc/nixos/hardware-configuration.nix hardware-configuration_nix-pi.nix > /dev/null ; then
+        diff /etc/nixos/hardware-configuration.nix hardware-configuration_nix-pi.nix
+        read -n 1 -s -r -p "Press any key to overwrite hardware-configuration_nix-pi.nix"
+        sudo cp -v /etc/nixos/hardware-configuration.nix hardware-configuration_nix-pi.nix
+    fi
+    
 fi
 
 if [ "$hn" == "nix-test" ]; then
-    sudo cp -v /etc/nixos/configuration.nix nix-test.nix
-    sudo cp -v /etc/nixos/hardware-configuration.nix hardware-configuration_nix-test.nix
+    if ! diff /etc/nixos/configuration.nix nix-test.nix > /dev/null ; then
+        diff /etc/nixos/configuration.nix nix-test.nix
+        read -n 1 -s -r -p "Press any key to overwrite nix-test.nix"
+        sudo cp -v /etc/nixos/configuration.nix nix-test.nix
+    fi
+    
+    if ! diff /etc/nixos/hardware-configuration.nix hardware-configuration_nix-test.nix > /dev/null ; then
+        diff /etc/nixos/hardware-configuration.nix hardware-configuration_nix-test.nix
+        read -n 1 -s -r -p "Press any key to overwrite hardware-configuration_nix-test.nix"
+        sudo cp -v /etc/nixos/hardware-configuration.nix hardware-configuration_nix-test.nix
+    fi
+    
 fi
--- nix_files/copy_to_etc_nixos.sh
+++ nix_files/copy_to_etc_nixos.sh
@@ -2,44 +2,145 @@
 
 hn=$(hostname)
 
-echo "Copying files for $hn"
+echo "Copying files for $hn (from this folder to etc)"
+echo "If you don't see further output, nothing changed."
 
 if [ "$hn" == "nix" ]; then
-    sudo cp -v nix-home.nix /etc/nixos/configuration.nix
-    sudo cp -v hardware-configuration_nix-home.nix /etc/nixos/
-    sudo cp -v packages.nix /etc/nixos/
-    sudo cp -v services.nix /etc/nixos/
-    sudo cp -v steam.nix /etc/nixos/
-    sudo cp -v pwn.nix /etc/nixos/
-    sudo cp -v k3s.nix /etc/nixos/
-    sudo cp -v temis.nix /etc/nixos/
-    sudo cp -v podman.nix /etc/nixos/
+    if ! diff nix-home.nix /etc/nixos/configuration.nix > /dev/null ; then
+        diff nix-home.nix /etc/nixos/configuration.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/nix-home"
+        sudo cp -v nix-home.nix /etc/nixos/configuration.nix
+    fi
+    if ! diff hardware-configuration_nix-home.nix /etc/nixos/hardware-configuration_nix-home.nix > /dev/null ; then
+        diff hardware-configuration_nix-home.nix /etc/nixos/hardware-configuration_nix-home.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/hardware-configuration_nix"
+        sudo cp -v hardware-configuration_nix-home.nix /etc/nixos/
+    fi
+    if ! diff packages.nix /etc/nixos/packages.nix > /dev/null ; then
+        diff packages.nix /etc/nixos/packages.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/packages.nix"
+        sudo cp -v packages.nix /etc/nixos/
+    fi
+    if ! diff services.nix /etc/nixos/services.nix > /dev/null ; then
+        diff services.nix /etc/nixos/services.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/services.nix"
+        sudo cp -v services.nix /etc/nixos/
+    fi
+    if ! diff steam.nix /etc/nixos/steam.nix > /dev/null ; then
+        diff steam.nix /etc/nixos/steam.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/steam.nix"
+        sudo cp -v steam.nix /etc/nixos/
+    fi
+    if ! diff pwn.nix /etc/nixos/pwn.nix > /dev/null ; then
+        diff pwn.nix /etc/nixos/pwn.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/pwn.nix"
+        sudo cp -v pwn.nix /etc/nixos/
+    fi
+    if ! diff k3s.nix /etc/nixos/k3s.nix > /dev/null ; then
+        diff k3s.nix /etc/nixos/k3s.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/k3s.nix"
+        sudo cp -v k3s.nix /etc/nixos/
+    fi
+    if ! diff temis.nix /etc/nixos/temis.nix > /dev/null ; then
+        diff temis.nix /etc/nixos/temis.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/temis.nix"
+        sudo cp -v temis.nix /etc/nixos/
+    fi
+    if ! diff podman.nix /etc/nixos/podman.nix > /dev/null ; then
+        diff podman.nix /etc/nixos/podman.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/podman.nix"
+        sudo cp -v podman.nix /etc/nixos/
+    fi
 fi
 
 if [ "$hn" == "tp-belo" ]; then
-    sudo cp -v nix-thinkpad.nix /etc/nixos/configuration.nix
-    sudo cp -v hardware-configuration_nix-thinkpad.nix /etc/nixos/
-    sudo cp -v packages.nix /etc/nixos/
-    sudo cp -v services.nix /etc/nixos/
-    sudo cp -v pwn.nix /etc/nixos/
-    sudo cp -v temis.nix /etc/nixos/
-    sudo cp -v k3s.nix /etc/nixos/
-    sudo cp -v podman.nix /etc/nixos/
+    if ! diff nix-thinkpad.nix /etc/nixos/configuration.nix > /dev/null ; then
+        diff nix-thinkpad.nix /etc/nixos/configuration.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/nix-thinkpad"
+        sudo cp -v nix-thinkpad.nix /etc/nixos/configuration.nix
+    fi
+    if ! diff hardware-configuration_nix-thinkpad.nix /etc/nixos/hardware-configuration_nix-thinkpad.nix > /dev/null ; then
+        diff hardware-configuration_nix-thinkpad.nix /etc/nixos/hardware-configuration_nix-thinkpad.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/hardware-configuration_nix"
+        sudo cp -v hardware-configuration_nix-thinkpad.nix /etc/nixos/
+    fi
+    if ! diff packages.nix /etc/nixos/packages.nix > /dev/null ; then
+        diff packages.nix /etc/nixos/packages.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/packages.nix"
+        sudo cp -v packages.nix /etc/nixos/
+    fi
+    if ! diff services.nix /etc/nixos/services.nix > /dev/null ; then
+        diff services.nix /etc/nixos/services.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/services.nix"
+        sudo cp -v services.nix /etc/nixos/
+    fi
+    if ! diff pwn.nix /etc/nixos/pwn.nix > /dev/null ; then
+        diff pwn.nix /etc/nixos/pwn.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/pwn.nix"
+        sudo cp -v pwn.nix /etc/nixos/
+    fi
+    if ! diff temis.nix /etc/nixos/temis.nix > /dev/null ; then
+        diff temis.nix /etc/nixos/temis.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/temis.nix"
+        sudo cp -v temis.nix /etc/nixos/
+    fi
+    if ! diff k3s.nix /etc/nixos/k3s.nix > /dev/null ; then
+        diff k3s.nix /etc/nixos/k3s.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/k3s.nix"
+        sudo cp -v k3s.nix /etc/nixos/
+    fi
+    if ! diff podman.nix /etc/nixos/podman.nix > /dev/null ; then
+        diff podman.nix /etc/nixos/podman.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/podman.nix"
+        sudo cp -v podman.nix /etc/nixos/
+    fi
 fi
 
 if [ "$hn" == "nix-vps" ]; then
-    sudo cp -v nix-vps.nix /etc/nixos/configuration.nix
-    sudo cp -v hardware-configuration_nix-vps.nix /etc/nixos/
-    sudo cp -v sa3_document_manager.nix /etc/nixos/
+    if ! diff nix-vps.nix /etc/nixos/configuration.nix > /dev/null ; then
+        diff nix-vps.nix /etc/nixos/configuration.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/nix-vps"
+        sudo cp -v nix-vps.nix /etc/nixos/configuration.nix
+    fi
+    if ! diff hardware-configuration_nix-vps.nix /etc/nixos/hardware-configuration_nix-vps.nix > /dev/null ; then
+        diff hardware-configuration_nix-vps.nix /etc/nixos/hardware-configuration_nix-vps.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/hardware-configuration_nix"
+        sudo cp -v hardware-configuration_nix-vps.nix /etc/nixos/
+    fi
+    if ! diff sa3_document_manager.nix /etc/nixos/sa3_document_manager.nix > /dev/null ; then
+        diff sa3_document_manager.nix /etc/nixos/sa3_document_manager.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/sa3_document_manager.nix"
+        sudo cp -v sa3_document_manager.nix /etc/nixos/
+    fi
 fi
 
 if [ "$hn" == "nix-pi" ]; then
-    sudo cp -v nix-pi.nix /etc/nixos/configuration.nix
-    sudo cp -v hardware-configuration_nix-pi.nix /etc/nixos/hardware-configuration.nix
+    if ! diff nix-pi.nix /etc/nixos/configuration.nix > /dev/null ; then
+        diff nix-pi.nix /etc/nixos/configuration.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/nix-pi"
+        sudo cp -v nix-pi.nix /etc/nixos/configuration.nix
+    fi
+    if ! diff hardware-configuration_nix-pi.nix /etc/nixos/hardware-configuration.nix > /dev/null ; then
+        diff hardware-configuration_nix-pi.nix /etc/nixos/hardware-configuration.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/hardware-configuration_nix"
+        sudo cp -v hardware-configuration_nix-pi.nix /etc/nixos/hardware-configuration.nix
+    fi
 fi
 
 if [ "$hn" == "nix-test" ]; then
-    sudo cp -v nix-test.nix /etc/nixos/configuration.nix
-    sudo cp -v hardware-configuration_nix-test.nix /etc/nixos/hardware-configuration.nix
-    sudo cp -v temis.nix /etc/nixos/
+    if ! diff nix-test.nix /etc/nixos/configuration.nix > /dev/null ; then
+        diff nix-test.nix /etc/nixos/configuration.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/nix-test"
+        sudo cp -v nix-test.nix /etc/nixos/configuration.nix
+    fi
+    if ! diff hardware-configuration_nix-test.nix /etc/nixos/hardware-configuration.nix > /dev/null ; then
+        diff hardware-configuration_nix-test.nix /etc/nixos/hardware-configuration.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/hardware-configuration_nix"
+        sudo cp -v hardware-configuration_nix-test.nix /etc/nixos/hardware-configuration.nix
+    fi
+    if ! diff temis.nix /etc/nixos/temis.nix > /dev/null ; then
+        diff temis.nix /etc/nixos/temis.nix
+        read -n 1 -s -r -p "Press any key to overwrite /etc/nixos/temis.nix"
+        sudo cp -v temis.nix /etc/nixos/
+    fi
 fi
