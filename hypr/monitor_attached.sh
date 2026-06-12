d079b8c6541406700c08aee7531d42ad49a9582a
Ben Lorenz <bnlrnz@gmail.com>
2026-06-12 09:57:02 +0200

    removed old hyprland configs, updated xdg portal script

--- hypr/monitor_attached.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-hyprctl dispatch moveworkspacetomonitor 1 "$1"
-hyprctl dispatch workspace 1
-hyprctl dispatch moveworkspacetomonitor 3 "$1"
-
-hyprctl dispatch dpms on
-pkill -f wpaperd; wpaperd &
-pkill -f waybar; waybar &
--- hypr/monitor_detached.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-pkill -f waybar; waybar &
-pkill -f wpaperd; wpaperd &
-
-hyprctl dispatch workspace 1
